f1e210f10f85bd2853855023fc8b3dbe4231f99d
clebemachado <clebmachado.em@gmail.com>
2022-07-14 11:00:05 -0300

    Metodo mudado

--- src/main/java/br/com/vermser/pessoapi/controller/PessoaCPFController.java
+++ src/main/java/br/com/vermser/pessoapi/controller/PessoaCPFController.java
@@ -3,10 +3,7 @@ package br.com.vermser.pessoapi.controller;
 import br.com.vermser.pessoapi.dto.pessoacpf.PessoaCPFDTO;
 import br.com.vermser.pessoapi.service.PessoaCPFService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -31,7 +28,7 @@ public class PessoaCPFController {
         return pessoaCPFService.getByCpf(cpf);
     }
 
-    @GetMapping("/delete/{idUser}")
+    @DeleteMapping("/delete/{idUser}")
     public void delete(@PathVariable Integer idUser) throws Exception {
         pessoaCPFService.delete(idUser);
     }
